afec7ba2e986b1c354eea9e99edc023140952330
Eric Johney <eric@ericjohney.com>
2020-01-09 17:19:16 -0800

    set default title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,7 +20,7 @@ export default class MyApp extends App {
     return (
       <React.Fragment>
         <Head>
-          <title>My page</title>
+          <title>mailthing</title>
           <meta
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width"
